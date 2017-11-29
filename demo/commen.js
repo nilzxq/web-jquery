aaca3250393e406a05cd3de857b8d2e8447e6112
nilzxq <2013358717@qq.com>
2017-11-29 14:58:22 +0800

    localstorge

--- demo/commen.js
+++ demo/commen.js
@@ -1,17 +1,20 @@
 var content = {};
 
+if(typeof(Storage)!=="undefined"){
+    function submitData(){
+        content.user = document.getElementById("user").value;
+        content.pwd = document.getElementById("passwd").value;
+        localStorage.setItem("content", JSON.stringify(content));
+    }
 
 
-function submitData(){
-    content.user = document.getElementById("user").value;
-    content.pwd = document.getElementById("passwd").value;     
-    localStorage.setItem("content", JSON.stringify(content));
+    function getData(){
+        content = JSON.parse(localStorage.getItem("content"));
+        console.log(content)
+        document.getElementById("username").value = content.user
+        document.getElementById("pwd").value = content.pwd
+    }
+}else{
+    document.getElementById("result").innerHTML = "抱歉！您的浏览器不支持 Web Storage ...";
 }
 
-
-function getData(){
-    content = JSON.parse(localStorage.getItem("content"));
-    console.log(content)
-    document.getElementById("username").value = content.user 
-    document.getElementById("pwd").value = content.pwd 
-}
